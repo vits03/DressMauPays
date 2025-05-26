391fa47e4bdf00bdf49e07f65e09044821816a28
vits03 <101546379+vits03@users.noreply.github.com>
2025-05-26 21:31:19 +0400

    test

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://dressmaupays.com", // ✅ Replace with your actual domain
-  generateRobotsTxt: true, // ✅ Also generate robots.txt
+  siteUrl: "https://dressmaupays.com",
+  generateRobotsTxt: true,
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 5000,
-  generateIndexSitemap: true, // For multi-sitemaps
+  generateIndexSitemap: true,
+  exclude: ["/admin"], // ⛔ Exclude this route from sitemap
 };
+
